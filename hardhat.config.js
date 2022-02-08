0a8b9fde618a91180edce3563832119c19f67c2f
Tomoya Nagasawa <ocrybit@gmail.com>
2022-02-08 13:58:30 +0900

    verify contract with polygonscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle")
-
+require("@nomiclabs/hardhat-etherscan")
+const secrets = require("./secrets.json")
 task("accounts", "Prints the list of accounts", async () => {
   const accounts = await ethers.getSigners()
 
@@ -10,4 +11,13 @@ task("accounts", "Prints the list of accounts", async () => {
 
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    polygon: {
+      url: secrets.polygon.url,
+      accounts: [secrets.polygon.key],
+    },
+  },
+  etherscan: {
+    apiKey: secrets.polygon.apiKey,
+  },
 }
